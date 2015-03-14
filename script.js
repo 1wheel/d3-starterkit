9a269497e85630e9f54dd275ff0acbfbfbb9ce63
Adam Pearce <1wheel@gmail.com>
2015-03-14 02:10:51 -0400

    jetpack update

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 d3.tsv('data.tsv', function(data){
-  var c = d3.conventions({parentSel: d3.select('#graph')})
+  c = d3.conventions({parentSel: d3.select('#graph')})
 
   c.x.domain(d3.extent(data, ƒ('sepalWidth')) ).nice()
   c.y.domain(d3.extent(data, ƒ('sepalLength'))).nice()
   c.drawAxis()
 
-  c.svg.appendData('circle', data)
-      .attrC('cx', c.x, ƒ('sepalWidth'))
-      .attrC('cy', c.y, ƒ('sepalLength'))
-      .attrC('fill', c.color, ƒ('species'))
+  c.svg.dataAppend(data, 'circle')
+      .attr('cx', ƒ('sepalWidth', c.x))
+      .attr('cy', ƒ('sepalLength',c.y))
+      .attr('fill', ƒ('species', c.color))
       .attr({r: 5, stroke: '#000'})
       .call(d3.attachTooltip)
+
+  var legend = c.svg.dataAppend(c.color.domain(), 'g.legend')
+      .translate(function(d, i){ return [0, i*20] })
+
+  legend.append('rect')
+      .attr({x: c.width - 18, width: 18, height: 18})
+      .style('fill', c.color)
+
+  legend.append('text')
+      .attr({x: c.width - 24, y: 9, dy: '.33em', 'text-anchor': 'end'})
+      .text(ƒ())
 })
